dd5dbac5a902e3f99b1d2f407f06504697d33b92
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2014-09-17 14:35:00 -0700

    works

--- thumbnail.py
+++ thumbnail.py
@@ -4,44 +4,36 @@ extension to pilbox server http://agschwender.github.io/pilbox/#extension
 """
 import tornado.gen
 from pilbox.app import PilboxApplication, ImageHandler, main
-from md5s3stash import md5_to_s3_url
+from md5s3stash import md5_to_http_url
 import boto
+import os
 
 
 class ThumbnailApplication(PilboxApplication):
     def get_handlers(self):
         # URL regex to handler mapping
-        return [(r"/(\d+)x(\d+)/(.+)/(.+)", ThumbnailImageHandler)]
-        #            w, h, bucket_base, md5
+        return [(r"/(\d+)x(\d+)/(.+)", ThumbnailImageHandler)]
+        #            w, h, md5
 
 
 class ThumbnailImageHandler(ImageHandler):
     def prepare(self):
         # TODO: default to clip, discard all other arguments
+        assert 'BUCKET_BASE' in os.environ, "`BUCKET_BASE` must be set"
         self.args = self.request.arguments.copy()
 
     @tornado.gen.coroutine
-    def get(self, w, h, bucket_base, md5):
-        uri = md5_to_s3_url(md5, bucket_base)
-        bucket_name, key_name = uri[len('s3://'):].split('/', 1)
-        c = boto.connect_s3()
-        bucket = c.get_bucket(bucket_name)
-        key = bucket.get_key(key_name)
+    def get(self, w, h, md5):
+        
+        url = md5_to_http_url(md5, os.environ['BUCKET_BASE'])
         self.args.update(dict(w=w, h=h, url=url))
-        # validate max size?
+
         self.validate_request()
-        self.render_image(key)
+        resp = yield self.fetch_image()
+        self.render_image(resp)
 
     def get_argument(self, name, default=None):
         return self.args.get(name, default)
-
-    def _validate_url(self):
-        url = self.get_argument("url")
-        if not url:
-            raise errors.UrlError("Missing url")
-        elif url.startswith("s3://"):
-            return
-        raise errors.UrlError("Unsupported protocol")
 
 
 if __name__ == "__main__":
