e192713e751eb7aa07d13c845a706d53145f097e
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-03-17 23:09:07 -0700

    update to thunbmail server
    die faster if not configured; set default image

--- thumbnail.py
+++ thumbnail.py
@@ -6,12 +6,16 @@ import tornado.gen
 from pilbox.app import PilboxApplication, ImageHandler, main
 from md5s3stash import md5_to_http_url
 import os
+assert 'BUCKET_BASE' in os.environ, "`BUCKET_BASE` must be set"
 
 
 class ThumbnailApplication(PilboxApplication):
     def get_handlers(self):
         # URL regex to handler mapping
-        return [(r"/([^/]+)/(\d+)x(\d+)/(.+)", ThumbnailImageHandler)]
+        return [
+            (r"^/([^/]+)/(\d+)x(\d+)/([a-fA-F\d]{32})$", ThumbnailImageHandler),
+            (r"^/([^/]+)/(\d+)x(\d+)/.*$", ThumbnailImageHandler)
+        ]
         #            mode, w, h, md5
 
 
@@ -19,18 +23,16 @@ class ThumbnailImageHandler(ImageHandler):
     def prepare(self):
         self.args = self.request.arguments.copy()
         self.settings['content_type_from_image'] = True
-        assert 'BUCKET_BASE' in os.environ, "`BUCKET_BASE` must be set"
 
     @tornado.gen.coroutine
-    def get(self, mode, w, h, md5):
-
+    def get(self, mode, w, h, md5='0d6cc125540194549459df758af868a8'):
         url = md5_to_http_url(md5, os.environ['BUCKET_BASE'])
         self.args.update(dict(w=w, h=h, url=url))
-
         self.validate_request()
         self.settings['mode'] = mode
         resp = yield self.fetch_image()
         self.render_image(resp)
+
 
     def get_argument(self, name, default=None):
         return self.args.get(name, default)
